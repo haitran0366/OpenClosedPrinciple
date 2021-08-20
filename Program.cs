a594ebd82ce8c641597cd26b23ec716388514113
haitran0366 <haitran_usa@yahoo.com>
2021-08-20 11:55:02 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OpenClosedPrinciple_ex_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Service service1 = new Service1();
+            service1.DoService();
+
+            Service service2 = new Service2();
+            service1.DoService();
+
+            Service service3 = new Service3();
+            service3.DoService();
+        }
+    }
+}
---
+++ Service1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosedPrinciple_ex_
+{
+    class Service1 : Service
+    {
+        public override void DoService()
+        {
+            Console.WriteLine("Do service 1");
+        }
+    }
+}
